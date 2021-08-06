13246efc601c0165e1749b11a387851c2249d104
annapetriga33 <annapetriga33@gmail.com>
2021-08-06 14:37:05 +0300

    first commit

---
+++ js-3(1)/js/custom.js
@@ -0,0 +1,6 @@
+var compareOfNumber = 5<2;
+if(Boolean(compareOfNumber)) {
+    alert('');
+} else{
+    alert('5 больше 2, а не наоборот');
+}
---
+++ js-3(2)/js/custom.js
@@ -0,0 +1,9 @@
+var kilometer = 1;
+var feet = 1;
+var kilometerTOMeters = kilometer * 1000;
+var feetTOMeters = feet * 0.305;
+if(kilometerTOMeters > feetTOMeters){
+    alert('В футах расстояние будет меньше чем в километрах');
+} else {
+    alert(' ');
+}
